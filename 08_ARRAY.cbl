9a3e8601cb930c6f40c114f3c69191e078781676
unknown <yoneeki1177@gmail.com>
2023-08-17 10:23:58 +0900

    cobol

--- 08_ARRAY.cbl
+++ 08_ARRAY.cbl
@@ -1,4 +1,4 @@
-       IDENTIFICATION DIVISION.
+ IDENTIFICATION DIVISION.
        PROGRAM-ID. 08_ARRAY.
 
        DATA DIVISION.
@@ -7,25 +7,21 @@
             03  A   PIC 99  OCCURS  5.
        01   I PIC 9 VALUE 1.
 
-       *******************************************
        PROCEDURE DIVISION.
        SiJak-P.
-           MOVE    1234567890  TO  BaeYul.
-           DISPLAY     A(1).
-           DISPLAY     A(2).
-           DISPLAY     A(3).
-           DISPLAY     A(4).
-           DISPLAY     A(5).
-           DISPLAY     BaeYul.
-           DISPLAY     '   '.
+           MOVE    1234567890   TO  BaeYul
+           DISPLAY 'Values after MOVE:'
+           PERFORM DISPLAY-A-VALUES
+           DISPLAY '-------------------'
+
+           MOVE    12345   TO  BaeYul
+           DISPLAY 'Values after another MOVE:'
+           PERFORM DISPLAY-A-VALUES
+           DISPLAY '-------------------'
        
-           MOVE    12345   TO  BaeYul.  
-           DISPLAY     A(1).
-           DISPLAY     A(2).
-           DISPLAY     A(3).
-           DISPLAY     A(4).
-           DISPLAY     A(5).
-           DISPLAY     BaeYul.
-           DISPLAY     ':'
-           
            STOP RUN.
+
+       DISPLAY-A-VALUES.
+           PERFORM VARYING I FROM 1 BY 1 UNTIL I > 5
+              DISPLAY 'A(' I ') : ' A(I)
+           END-PERFORM.
